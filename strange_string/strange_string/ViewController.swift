90875a26b284a886d88938526b418c2c10f2ccaf
p5syche49 <u.p5yche49@gmail.com>
2017-10-29 12:33:37 +0900

    init project

---
+++ strange_string/strange_string/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  strange_string
+//
+//  Created by rose on 2017. 10. 29..
+//  Copyright © 2017년 rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
