9498a6693ca1dec92671eb4433772b2186d0e5b3
p5yche49 <u.p5yche49@gmail.com>
2017-11-26 11:23:15 +0900

    strangeString

--- strange_string/strange_string/ViewController.swift
+++ strange_string/strange_string/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var returnString = self.convertString(originalString: "tRy StragE stRing helLO wOrld")
+        print(returnString)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    func convertString(originalString:String) -> String {
+        let fullString = originalString
+        let fullStringArr = fullString.components(separatedBy: " ")
+        var returnString = ""
+        
+        for var tempString:String in fullStringArr {
+            var count:Int = 0
+            while count < tempString.characters.count {
+                var index = tempString.index(tempString.startIndex, offsetBy:count)
+                var tempChar:String
+                if count%2 == 0 {
+                    tempChar = String(tempString[index]).uppercased()
+                } else {
+                    tempChar = String(tempString[index]).lowercased()
+                }
+                returnString = returnString + tempChar
+                count = count + 1
 
+            }
+            returnString = returnString + " "
+        }
+        
+        
+        return returnString
+    }
 }
-
